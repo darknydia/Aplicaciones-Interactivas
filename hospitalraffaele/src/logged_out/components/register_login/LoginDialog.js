acb93efe70383db6c0758c999ce4fe06f5b132ff
nydia <>
2020-08-13 04:11:29 -0300

    Se crean las variables globals y validadaor de usuario, para validacion de roles.

--- hospitalraffaele/src/logged_out/components/register_login/LoginDialog.js
+++ hospitalraffaele/src/logged_out/components/register_login/LoginDialog.js
@@ -15,6 +15,8 @@ import HighlightedInformation from "../../../shared/components/HighlightedInform
 import ButtonCircularProgress from "../../../shared/components/ButtonCircularProgress";
 import VisibilityPasswordTextField from "../../../shared/components/VisibilityPasswordTextField";
 
+import global from "../Global.js";
+
 const styles = (theme) => ({
   forgotPassword: {
     marginTop: theme.spacing(2),
@@ -48,26 +50,51 @@ function LoginDialog(props) {
   const [isLoading, setIsLoading] = useState(false);
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const loginEmail = useRef();
-  const loginPassword = useRef();
+  const loginPassword = useRef(); 
 
   const login = useCallback(() => {
     setIsLoading(true);
     setStatus(null);
-    if (loginEmail.current.value !== "test@web.com") {
-      setTimeout(() => {
-        setStatus("invalidEmail");
-        setIsLoading(false);
-      }, 1500);
-    } else if (loginPassword.current.value !== "test") {
-      setTimeout(() => {
-        setStatus("invalidPassword");
-        setIsLoading(false);
-      }, 1500);
-    } else {
-      setTimeout(() => {
-        history.push("/c/dashboard");
-      }, 150);
-    }
+
+      global.usuarioElegido = global.usuarioRoles.filter(x=>{
+        
+        if (loginEmail.current.value !== x.email) {
+          setTimeout(() => {
+            setStatus("invalidEmail");
+            setIsLoading(false);
+          }, 1500);
+        } else if (loginPassword.current.value !== x.pass) {
+          setTimeout(() => {
+            setStatus("invalidPassword");
+            setIsLoading(false);
+          }, 1500);
+        } else {
+          setTimeout(() => {
+            history.push("/c/dashboard");
+          }, 150);
+        }
+        
+        if ( x.email === loginEmail.current.value && x.pass === loginPassword.current.value) 
+        {
+          return x;
+        }       
+      });
+
+    // if (loginEmail.current.value !== "test@web.com") {
+    //   setTimeout(() => {
+    //     setStatus("invalidEmail");
+    //     setIsLoading(false);
+    //   }, 1500);
+    // } else if (loginPassword.current.value !== "test") {
+    //   setTimeout(() => {
+    //     setStatus("invalidPassword");
+    //     setIsLoading(false);
+    //   }, 1500);
+    // } else {
+    //   setTimeout(() => {
+    //     history.push("/c/dashboard");
+    //   }, 150);
+    // }
   }, [setIsLoading, loginEmail, loginPassword, history, setStatus]);
 
   return (
